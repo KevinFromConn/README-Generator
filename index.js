dea745a15d9e77386183e70c8ac4bb24edc4e35f
KevinFromConn <kfc21194@gmail.com>
2021-09-07 19:29:56 -0400

    added questions to inquirer.prompt

--- index.js
+++ index.js
@@ -1,7 +1,79 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const { async } = require("rxjs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const userPrompt = async () => {
+  const response = await inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the title of your page?",
+      name: "Title",
+    },
+    {
+      type: "input",
+      message: "Please give a description of your project.",
+      name: "Description",
+    },
+    {
+      type: "input",
+      message: "Installation Instructions",
+      name: "Installation",
+      default: "No installation information is available.",
+    },
+    {
+      type: "input",
+      message: "Usage Information",
+      name: "Usage",
+      default: "No usage information is available.",
+    },
+    {
+      type: "list",
+      name: "License",
+      message: "Please choose a License for your application.",
+      choices: ["MIT", "ISC", "Unlicensed"],
+    },
+    {
+      type: "input",
+      message: "Contribution guidelines",
+      name: "Contribution",
+      default: "No contribution information is available.",
+    },
+    {
+      type: "input",
+      message: "Test Instructions",
+      name: "Test",
+      default: "No testing information is available.",
+    },
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "GitHub",
+      filter: function (answers) {
+        if (answers) {
+          return `https://github.com/${answers}`;
+        } else {
+          return "No GitHub link was entered.";
+        }
+      },
+    },
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "Email",
+      filter: function (answers) {
+        if (answers) {
+          return `${answers}`;
+        } else {
+          return "No email address was entered.";
+        }
+      },
+    },
+  ]);
+};
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
